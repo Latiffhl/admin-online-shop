e6c0eef10d8843230d4f65af915770d6c527d4b1
Latif Falikal <131035184+Latiffhl@users.noreply.github.com>
2025-03-26 16:52:51 +0700

    update03-26
    fix bug pada database yang tidak bisa terhubung ke params.stroreId

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
+// Tentukan rute publik
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/public(.*)']);
 
 export default clerkMiddleware(async (auth, request) => {
+  console.log('Request URL:', request.url);
+
   if (!isPublicRoute(request)) {
+    console.log('Protected route, authenticating...');
     await auth.protect();
+  } else {
+    console.log('Public route, no authentication required.');
   }
-  publicRoutes: ['/api/:path'];
 });
 
 export const config = {
